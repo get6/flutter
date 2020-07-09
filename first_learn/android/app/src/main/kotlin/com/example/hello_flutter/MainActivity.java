66878d4904cd50d5d0a2c1dc7905249c9506ad7f
get6 <ittae.com@gmail.com>
2020-07-09 23:31:48 +0900

    doing channel

--- first_learn/android/app/src/main/kotlin/com/example/hello_flutter/MainActivity.java
+++ first_learn/android/app/src/main/kotlin/com/example/hello_flutter/MainActivity.java
@@ -1,25 +1,43 @@
 package com.example.hello_flutter;
 
+import android.Manifest;
 import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.BatteryManager;
 import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+
+    private static final String TAG = "PlatformChannels";
+
     private static final String CHANNEL_BATTERY = "android/battery";
     private static final String METHOD_BATTERY = "getBatteryLevel";
+    private static final String METHOD_CURRENT_LOCATION = "getCurrentLocation";
+    private static final String CHANNEL_LOCATION = "android/location";
+
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-        super.configureFlutterEngine(flutterEngine);
+
+        Log.d(TAG, "configureFlutterEngine()");
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+
         new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL_BATTERY)
                 .setMethodCallHandler(
                         (call, result) -> {
@@ -32,6 +50,10 @@ public class MainActivity extends FlutterActivity {
                                 } else {
                                     result.error("UNAVAILABLE", "Battery level not available.", null);
                                 }
+                            } else if (call.method.equals(METHOD_CURRENT_LOCATION)) {
+                                Log.d(TAG, "location_channel :: method " + call.method);
+                                fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+                                getCurrentLocation(result);
                             } else {
                                 result.notImplemented();
                             }
@@ -52,5 +74,25 @@ public class MainActivity extends FlutterActivity {
         }
 
         return batteryLevel;
+    }
+
+    private void getCurrentLocation(MethodChannel.Result result) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, location -> {
+            if (location != null) {
+                String res = "(" + location.getLatitude() + ", " + location.getLongitude() + ")";
+                Log.d(TAG, "location? " + res);
+                result.success(res);
+            }
+        });
     }
 }
