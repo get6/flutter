07ad63fba4f601924f235f4143861c8eaee9ec58
get6 <ittae.com@gmail.com>
2020-07-16 23:42:45 +0900

    add projects

---
+++ projects/chapter07/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // primarySwatch: Colors.teal,
+        primaryColor: Colors.white
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text('복잡한 UI'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.add), onPressed: () {
+
+          }),
+        ],
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          '$_index 페이지',
+          style: TextStyle(
+            fontSize: 40,
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            _index = index;
+          });
+        },
+        currentIndex: _index,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            title: Text('홈'),
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            title: Text('이용서비스'),
+            icon: Icon(Icons.assignment),
+          ),
+          BottomNavigationBarItem(
+            title: Text('내 정보'),
+            icon: Icon(Icons.account_circle),
+          ),
+        ],
+      ),
+    );
+  }
+}
